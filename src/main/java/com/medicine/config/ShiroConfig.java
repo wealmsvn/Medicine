e61eb62e48ca05781bfda6cdfa3a330d4087b5ca
mfx <mfx@at.com>
2019-10-22 20:21:32 +0800

    中医药第二次上传

---
+++ src/main/java/com/medicine/config/ShiroConfig.java
@@ -0,0 +1,45 @@
+package com.medicine.config;
+
+import com.medicine.shiro.MyRealm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ShiroConfig {
+
+//    权限管理对象
+    @Bean
+    public DefaultWebSecurityManager creatSr(MyRealm realm){
+
+        return new DefaultWebSecurityManager(realm);
+    }
+
+    @Bean
+    public ShiroFilterFactoryBean creatSF(DefaultWebSecurityManager manager){
+  //创建过滤器对象
+        ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
+  //        设置过滤的权限
+        factoryBean.setSecurityManager(manager);
+  // 设置需要过滤的页面
+        factoryBean.setLoginUrl("login.html");
+//        登陆的接口
+        factoryBean.setSuccessUrl("index.html");
+//        未授权的页面
+        factoryBean.setUnauthorizedUrl("error.html");
+//        /4、设置拦截规则 anon：匿名访问 放行 谁都可以使用 authc:认证访问 必须登录才可以使用
+
+        Map map= new HashMap();
+        map.put("/login.html","anon");
+        map.put("/user/login.do","anon");
+//        静态资源
+        map.put("/static/media/*","anon");
+        map.put("/*","authc");
+        factoryBean.setFilterChainDefinitionMap(map);
+        return factoryBean;
+    }
+}
